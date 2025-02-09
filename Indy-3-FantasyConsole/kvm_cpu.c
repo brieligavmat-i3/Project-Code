e3d43cf6360bb7515277f9a4a3dcaa1b972e5c90
Matthew Watson <capnkraken016@gmail.com>
2025-02-09 17:47:02 -0500

    Worked on CPU
    Not tested yet.
    But I'm working on instruction decode and classification.

--- Indy-3-FantasyConsole/kvm_cpu.c
+++ Indy-3-FantasyConsole/kvm_cpu.c
@@ -2,17 +2,191 @@
 *	Author: Matthew Watson
 */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "leakcheck_util.h"
+
 #include "kvm_cpu.h"
+
+static uint8_t extract_bits(uint8_t target, uint8_t mask, uint8_t shift) {
+	return (target & mask) >> shift;
+}
 
 uint8_t kvm_cpu_fetch_byte(kvm_memory* mem, size_t index) {
 	return kvm_memory_get_byte(mem, index);
 }
 
+#pragma region Helper functions for decoding.
+
+static void instr_reset_defaults(kvm_instruction* out_instr) {
+	out_instr->instruction_size = kvms_invalid;
+	out_instr->addressing_mode = kvma_invalid;
+	out_instr->instruction_class = kvmc_invalid;
+	out_instr->register_operand = kvmr_invalid;
+
+	out_instr->byte1 = 0;
+	out_instr->byte2 = 0;
+}
+
+// Call this first.
+static void decode_instr_size(uint8_t instruction, kvm_instruction* out_instr) {
+	uint8_t pattern = extract_bits(instruction, 0b11000000, 6);
+	switch (pattern) {
+	case 0b00:			// 00******
+		out_instr->instruction_size = kvms_small;
+		break;
+	case 0b01:			// 01******
+		out_instr->instruction_size = kvms_med;
+		break;
+	case 0b10:			// 10******
+		out_instr->instruction_size = kvms_large;
+		break;
+	case 0b11:			// 11******
+	{
+		pattern = extract_bits(instruction, 0b00100000, 5);
+		if (pattern == 0b01) {	// 111*****
+			out_instr->instruction_size = kvms_large;
+		}
+		else {					// 110*****
+			out_instr->instruction_size = kvms_med;
+		}
+	}
+	break;
+	default:
+		fprintf(stderr, "Error with kvm_cpu.c, decode_instr_size(): Pattern \"%x\" is not matched.", pattern);
+		break;
+	}
+}
+
+// Call this AFTER decode_instr_size(), when out_instr should contain the size.
+static void decode_instr_addressing_mode(uint8_t instruction, kvm_instruction* out_instr) {
+	uint8_t pattern = 0;
+	
+	switch (out_instr->instruction_size) {
+	case kvms_small:
+		out_instr->addressing_mode = kvma_implicit; // The only one-byte addressing mode.
+		break;
+	case kvms_med:
+	{
+		pattern = extract_bits(instruction, 0b11100000, 5);
+
+		switch (pattern) {
+		case 0b010:	// Standard zero-page.
+			out_instr->addressing_mode = kvma_zeropage;
+			break;
+		case 0b011: // Zero page, x (or y)
+		{
+			pattern = extract_bits(instruction, 0b00011111, 0);
+			switch (pattern) {
+			case 0b10001:	// LDX $,y
+				out_instr->addressing_mode = kvma_zpy;
+				break;
+			case 0b10101:	// STX $,y
+				out_instr->addressing_mode = kvma_zpy;
+				break;
+			default:		// All others are z.p., x.
+				out_instr->addressing_mode = kvma_zpx;
+				break;
+			}
+		}
+		break;
+		case 0b110:
+			// 0v11011000 is the lowest opcode representing a relative addr. mode in this category.
+			if (instruction < 0b11011000) {
+				out_instr->addressing_mode = kvma_immediate;
+			}
+			else {
+				out_instr->addressing_mode = kvma_relative;
+			}
+			break;
+		default:
+			fprintf(stderr, "Error with kvm_cpu.c, decode_instr_addressing_mode(): Bit mask 0b11100000 \"%x\" is not matched.", pattern);
+			break;
+		}
+		break;
+	}
+	case kvms_large:
+	{
+		pattern = extract_bits(instruction, 0b11100000, 5);
+		if (pattern == 0b111) {	// Absolute addressing
+			out_instr->addressing_mode = kvma_absolute;
+		}else{ //0b10****** opcodes
+			pattern = extract_bits(instruction, 0b00110000, 4);
+			if (pattern == 0b10) { // x and y indirects
+				pattern = extract_bits(instruction, 0b00001000, 3);
+				if (pattern == 0b1) {	// indirect indexed, y
+					out_instr->addressing_mode = kvma_yind;
+				}
+				else {					// indexed indirect, x
+					out_instr->addressing_mode = kvma_indx;
+				}
+			}
+			else {
+				pattern = extract_bits(instruction, 0b00111111, 0);
+				if (pattern > 0b110100 || pattern == 0b010001 || pattern == 0b010011) { // absolute, y
+					out_instr->addressing_mode = kvma_aby;
+				}
+				else if(pattern == 0b001001){ // JMP instruction, indirect addr. mode.
+					out_instr->addressing_mode = kvma_indirect;
+				}
+				else {
+					out_instr->addressing_mode = kvma_abx;
+				}
+			}
+		}
+	}
+		break;
+	default:
+		fprintf(stderr, "Error with kvm_cpu.c, decode_instr_addressing_mode(): Instruction size \"%d\" is not matched.", out_instr->instruction_size);
+		break;
+	}
+}
+
+#pragma endregion
+
+void kvm_cpu_decode_instr(uint8_t instruction, kvm_instruction* out_instr){
+	
+	instr_reset_defaults(out_instr);
+
+	decode_instr_size(instruction, out_instr);
+	decode_instr_addressing_mode(instruction, out_instr);
+}
+
+
 void kvm_cpu_cycle(kvm_cpu* cpu, kvm_memory* mem) {
+	uint16_t pc = cpu->program_counter;
+
+	uint8_t current_opcode = kvm_cpu_fetch_byte(mem, pc++);
+
+	kvm_cpu_decode_instr(current_opcode, cpu->current_instruction);
 	/*
 	Fetch byte at program counter for opcode,
 	Decode opcode 
 	Based on instruction type, fetch the next 0, 1, or 2 bytes, and increment the program counter that much.
 	Execute the proper instruction.
 	*/
+}
+
+kvm_cpu* kvm_cpu_init(void) {
+	kvm_cpu* cpu = malloc(sizeof(kvm_cpu));
+	cpu->accumulator = 0;
+	cpu->x_index = 0;
+	cpu->y_index = 0;
+
+	cpu->stack_ptr = 0;
+
+	cpu->program_counter = PROGRAM_COUNTER_ENTRY_POINT;
+
+	cpu->processor_status = DEFAULT_PROCESSOR_STATUS;
+
+	kvm_instruction* instruction = malloc(sizeof(kvm_instruction));
+	instr_reset_defaults(instruction);
+}
+
+void kvm_cpu_free(kvm_cpu* cpu) {
+	if (cpu) {
+		if (cpu->current_instruction) free(cpu->current_instruction);
+		free(cpu);
+	}
 }
--- Indy-3-FantasyConsole/kvm_cpu.h
+++ Indy-3-FantasyConsole/kvm_cpu.h
@@ -20,6 +20,81 @@ a, x, y, stack, PC
 
 */
 
+// In the memory map, 0xE000 is the start point for program ROM information.
+#define PROGRAM_COUNTER_ENTRY_POINT 0xE000
+#define DEFAULT_PROCESSOR_STATUS 0
+
+typedef enum kvm_instruction_size {
+	kvms_small = 1, kvms_med = 2, kvms_large = 3, 
+	kvms_invalid = 0	// Used for error handling
+}kvm_instruction_size;
+
+typedef enum kvm_addressing_mode {
+	kvma_invalid,		// Used for error handling
+
+	kvma_implicit,		// These instructions only need one byte
+	kvma_immediate,		// Specify a numeric litereal to operate on
+	kvma_relative,		// Specify an offset to the program counter for branch instructions
+	kvma_zeropage,		// Absolute addressing but with one-byte addresses
+	kvma_zpx,			// Zero Page, but using x register as offset
+	kvma_zpy,			// Same as above, but using y register
+	kvma_absolute,		// Specify two-byte address to operate on
+	kvma_indirect,		// Specify two-byte address which stores the low byte of an address to jump to
+	kvma_abx,			// Absolute, but using x register as offset
+	kvma_aby,			// Same as above, but using y register
+	kvma_indx,			// Same as abx, except the value given is the low byte of a pointer to the target address
+	kvma_yind			// Specify two-byte address storing the low byte of a target address. Add the y register to the target.
+} kvm_addressing_mode;
+
+//TODO: define instruction classes (AND, ADC, JMP, etc.)
+typedef enum kvm_instruction_class {
+	kvmc_invalid,
+	kvmc_no_op, kvmc_force_interrupt, kvmc_return, 
+	kvmc_transfer, kvmc_transfer_accumulator, 
+	kvmc_stack_push, kvmc_stack_pull,
+	kvmc_set_flag, kvmc_clear_flag,
+
+	kvmc_and, kvmc_or, kvmc_xor, kvmc_bit_test,
+	kvmc_add, kvmc_subtract, kvmc_compare,
+
+	kvmc_increment, kvmc_decrement,
+	kvmc_shift_left, kvmc_shift_right,
+	kvmc_rotate_left, kvmc_rotate_right,
+
+	kvmc_load, kvmc_store,
+
+	kvmc_branch_if_clear, kvm_branch_if_set,
+	kvmc_jump, kvmc_jump_to_subroutine
+
+}kvm_instruction_class;
+
+typedef enum kvm_register_operand {
+	kvmr_invalid,
+
+	// Registers
+	kvmr_accumulator,
+	kvmr_x_index,
+	kvmr_y_index,
+	kvmr_stack_ptr,
+	kvmr_processor_status,
+
+	// Flags (stored in processor status)
+	kvmr_flag_carry,
+	kvmr_flag_overflow,
+	kvmr_flag_zero,
+	kvmr_flag_negative,
+	kvmr_flag_interrupt_disable
+}kvm_register_operand;
+
+typedef struct kvm_instruction {
+	kvm_instruction_size instruction_size;
+	kvm_addressing_mode addressing_mode;
+	kvm_instruction_class instruction_class;
+	kvm_register_operand register_operand;
+
+	uint8_t byte1, byte2; // Byte 1 is used with instruction sizes kvmi_med and kvmi_large, while byte 2 is only used with kvmi_large.
+}kvm_instruction;
+
 typedef struct kvm_cpu {
 	uint16_t program_counter;
 
@@ -28,12 +103,22 @@ typedef struct kvm_cpu {
 	uint8_t y_index;
 
 	uint8_t stack_ptr;
+
+	uint8_t processor_status;
+
+	kvm_instruction* current_instruction;
 } kvm_cpu;
 
 uint8_t kvm_cpu_fetch_byte(kvm_memory* mem, size_t index);
 
+void kvm_cpu_decode_instr(uint8_t instruction, kvm_instruction* out_instr);
 
 /* Perform fetch, decode, and execute operations.
 */
 void kvm_cpu_cycle(kvm_cpu* cpu, kvm_memory* mem);
+
+// Initializes the CPU and zeroes out its values.
+kvm_cpu* kvm_cpu_init(void);
+
+void kvm_cpu_free(kvm_cpu* cpu);
 
