0686c516e201274c51bf8297b3c150b0681c385a
Matthew Watson <capnkraken016@gmail.com>
2025-03-18 17:25:16 -0400

    Update kvm.h

--- INDY-3/vm-backend/kvm.h
+++ INDY-3/vm-backend/kvm.h
@@ -12,14 +12,19 @@
 So, we have to take in a filename to assemble, then assemble, and run it until it's done.
 */
 
+// Call this first
 int kvm_init(void);
 
+// Call this second with the filename of the currently loaded assembly file.
 // Runs the assembler python script, and loads the resulting file into ROM
 int kvm_load_instructions(const char* filename);
 
-// Run the VM. If max_cycles is > 0, The CPU will force quit after that many cycles.
+// Call this third.
+// Run the VM. If max_cycles is > 0, The CPU will force quit after that many cycles (set it to zero to ignore this).
 // max_cycles can also be set by a system call. (id 4)
 int kvm_start(int max_cycles);
+
+// Call this last, after kvm_start() returns
 int kvm_quit(void);
 
 void kvm_hexdump(int start_page, int page_count, bool print_cpu_status);
